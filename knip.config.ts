b84560b89dc38b1b89838eb5ef119b9664eae897
Yelyzaveta Veis <liza.veis14@gmail.com>
2025-07-17 16:50:40 +0300

    ci: fix ci actions ss-1

--- knip.config.ts
+++ knip.config.ts
@@ -4,6 +4,7 @@ const config: KnipConfig = {
 	workspaces: {
 		".": {
 			entry: ["./dangerfile.ts"],
+			ignore: ["apps/build/**"],
 		},
 		"apps/backend": {
 			entry: ["src/db/migrations/*.ts", "knexfile.ts"],
